c15d00577dc57c4e1743b03e10927687a0767c0f
ethansmith2000 <ethansmith2000@gmail.com>
2024-06-09 22:33:36 +0000

    chunk random

--- models/sparse.py
+++ models/sparse.py
@@ -2,22 +2,28 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-
-# id like to do a random chunk-wise rearrange... to sort of preserve part of the dot products.
-
 class PermuteIn(nn.Module):
 
     def __init__(self, 
                 dim,
-                mode="random", # random, roll 
+                heads,
+                mode="random", # random, roll, chunk_random
                 roll=0.4,
+                chunks=4, # must divide the chunk dim evenly
                 ):
         super().__init__()
-        roll = int(roll * dim)
+        full_dim = dim * heads
+        roll = int(roll * full_dim)
         if mode == "random":
-            permute = torch.randperm(dim)
+            permute = torch.randperm(full_dim)
+        elif mode == "roll":
+            permute = torch.roll(torch.arange(full_dim), roll)
+        elif mode == "chunk_random":
+            assert dim % chunks == 0, "chunks must divide the dim evenly"
+            chunk_indices = torch.randperm(full_dim // (dim // chunks))
+            permute = torch.cat([torch.arange((dim // chunks)) + i * (dim // chunks) for i in chunk_indices])
         else:
-            permute = torch.roll(torch.arange(dim), roll)
+            raise NotImplementedError("mode not implemented")
         self.register_buffer("permute", permute)
 
     def forward(self, x):
@@ -36,6 +42,88 @@ class Unpermute(nn.Module):
     def forward(self, x):
         return x[:, self.unperm]
 
+class BiasAdd(nn.Module):
+    
+    def __init__(self, dim):
+        super().__init__()
+        self.bias = nn.Parameter(torch.zeros(dim))
+
+    def forward(self, x):
+        return x + self.bias
+
+class SparseLinear(nn.Module):
+    
+    def __init__(self, in_dim=64, out_dim=64, heads=8, bias=True):
+        super(SparseLinear, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.h = heads
+        self.weight = nn.Parameter(torch.randn(heads, in_dim, out_dim))
+        self.bias_add = BiasAdd(out_dim) if bias else nn.Identity()
+
+    def forward(self, x):
+        # b, h * in_dim
+        b, h, in_dim = x.shape[0], self.h, self.in_dim
+        x = x.reshape(b, h, in_dim).reshape(b, h, -1, in_dim)
+        x = torch.vmap(lambda x: torch.bmm(x, self.weight))(x)
+        x = x.squeeze(-2).reshape(b, h, * self.od)
+        x = self.bias_add(x)
+        return x
+
+
+
+class SparseMLP(nn.Module):
+
+    def __init__(self, dim=64, 
+                        heads=8, 
+                        act=nn.GELU, 
+                        mlp_dim=256, 
+                        unperm=False, 
+                        residual=False, 
+                        dropout=0., 
+                        permute_mode="chunk_random", # ["random", "roll", "chunk_random", "linear"]
+                        ):
+        super(SparseMLP, self).__init__()
+        self.d = dim
+        self.h = heads
+        self.residual = lambda x, y: x + y if residual else x
+
+        self.up = nn.Parameter(torch.randn(heads, dim, mlp_dim))
+        self.down = nn.Parameter(torch.randn(heads, mlp_dim, dim))
+        self.act = act()
+
+        self.unperm = nn.Identity()
+        if permute_mode != "linear":
+            self.perm = PermuteIn(dim, heads, mode=permute_mode)
+            if unperm:
+                self.unperm = Unpermute(self.perm.permute)
+        else:
+            self.perm = nn.Linear(dim * heads, dim * heads)
+            if unperm:
+                self.unperm = nn.Linear(dim * heads, dim * heads)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        residual = x
+        b, h, d = x.shape[0], self.h, self.d
+
+        x = self.perm(x) # reorder features to have different interactions
+        x = x.reshape(b, h, d).reshape(b, h, -1, d) # b*h, 1, i bmm is annoying this way
+        # x = torch.bmm(x, self.up.repeat(b, 1, 1))
+        x = torch.vmap(lambda x: torch.bmm(x, self.up))(x)
+        x = self.act(x)
+        x = self.dropout(x)
+        # x = torch.bmm(x, self.down.repeat(b, 1, 1)) # b*h, 1, d
+        x = torch.vmap(lambda x: torch.bmm(x, self.down))(x)
+        x = self.dropout(x)
+        x = x.squeeze(-2).reshape(b, h * d)
+        x = self.unperm(x)
+
+        x = self.residual(x, residual)
+
+        return x
+
 
 class SparseFeedForward(nn.Module):
 
@@ -52,7 +140,7 @@ class SparseFeedForward(nn.Module):
 
         self.unperm = nn.Identity()
         if perm:
-            self.perm = PermuteIn(dim * heads)
+            self.perm = PermuteIn(dim, heads)
             if unperm:
                 self.unperm = Unpermute(self.perm.permute)
         else:
@@ -121,50 +209,6 @@ class SparseFeedForward(nn.Module):
 
 
 
-class SparseMLP(nn.Module):
-
-    def __init__(self, dim=64, heads=8, act=nn.GELU, mlp_dim=256, unperm=False, residual=False, dropout=0., perm=True):
-        super(SparseMLP, self).__init__()
-        self.d = dim
-        self.h = heads
-
-        self.residual = lambda x, y: x + y if residual else x
-
-        self.up = nn.Parameter(torch.randn(heads, dim, mlp_dim))
-        self.down = nn.Parameter(torch.randn(heads, mlp_dim, dim))
-        self.act = act()
-
-        self.unperm = nn.Identity()
-        if perm:
-            self.perm = PermuteIn(dim * heads)
-            if unperm:
-                self.unperm = Unpermute(self.perm.permute)
-        else:
-            self.perm = nn.Linear(dim * heads, dim * heads)
-            if unperm:
-                self.unperm = nn.Linear(dim * heads, dim * heads)
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        residual = x
-        b, h, d = x.shape[0], self.h, self.d
-
-        x = self.perm(x) # reorder features to have different interactions
-        x = x.reshape(b, h, d).reshape(b, h, -1, d) # b*h, 1, i bmm is annoying this way
-        # x = torch.bmm(x, self.up.repeat(b, 1, 1))
-        x = torch.vmap(lambda x: torch.bmm(x, self.up))(x)
-        x = self.act(x)
-        x = self.dropout(x)
-        # x = torch.bmm(x, self.down.repeat(b, 1, 1)) # b*h, 1, d
-        x = torch.vmap(lambda x: torch.bmm(x, self.down))(x)
-        x = self.dropout(x)
-        x = x.squeeze(1).reshape(b, h * d)
-        x = self.unperm(x)
-
-        x = self.residual(x, residual)
-
-        return x
 
 
 # class Network(nn.Module):
